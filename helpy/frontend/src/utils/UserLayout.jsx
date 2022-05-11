8bb9a3920d917c7c1c18e9f01921a5dfeba86c55
grecuandrei <grecuspider@gmail.com>
2022-05-11 23:24:04 +0300

    FE| Small fix

--- helpy/frontend/src/utils/UserLayout.jsx
+++ helpy/frontend/src/utils/UserLayout.jsx
@@ -5,33 +5,18 @@ import { authSettings } from "../AuthSettings";
 import UserHeader from "./UserHeader";
 import logo from "../assets/logo.svg";
 
-const UserLayout = ({ children }) => {
+const UserLayout = ({ isPublisher, children }) => {
   const { user } = useAuth0();
   const navigate = useNavigate();
 
   /* inseamna ca are admin ca si rol, deci nu poate vedea partea de utilizator */
   useEffect(() => {
-    const callBackendAPI = async () => {
-      const response = await fetch(`http://localhost:8000/api/users/register/${user.sub}`);
-      const body = await response.json();
-  
-      if (response.status !== 200) {
-        throw Error(body.message)
-      }
-      return body;
-    };
-
-    callBackendAPI()
-    .then(res => {
-      console.log(res)
-      if (res.isPublisher) navigate("/ads");
-    })
-    .catch(err => console.log(err));
+    if (isPublisher) navigate("/ads");
     // console.log(user)
     // if (user && user[authSettings.rolesKey].length === 1) {
     //   navigate("/ads");
     // }
-  }, [navigate, user.sub]);
+  }, [isPublisher, navigate]);
 
   return (
     <div className="user-layout">
