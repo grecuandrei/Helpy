b8f65becf18ad03b9ae4a7756bd51ef4c13c5b03
Grecu Andrei-George <50420491+grecuandrei@users.noreply.github.com>
2022-05-25 16:49:55 +0300

    Removed access keys to my own Auth0 for public usage

--- helpy/frontend/src/AuthSettings.js
+++ helpy/frontend/src/AuthSettings.js
@@ -1,8 +1,8 @@
 const authSettings = {
-  rolesKey: "https://helpy-api.com/roles",
-  domain: "dev-50lv5og2.us.auth0.com",
-  clientId: "PJ1bzIbu0eH4F9HSD8ovrqxKbbj17qeA",
-  audience: "https://helpy-api.com"
+  rolesKey: "your roles key url (typically audience/roles)",
+  domain: "your domain",
+  clientId: "your clientId",
+  audience: "your audience",
 };
 
 export { authSettings };
